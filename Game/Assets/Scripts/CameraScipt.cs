6ed6f93dbb10715f8aacda76522476253dd72e48
Alexandr <9aq360zqmuf1@mail.ru>
2021-04-13 10:00:45 +0500

    add unity project

---
+++ Game/Assets/Scripts/CameraScipt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScipt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, 1, -10);
+    }
+}
