d2c1ae14d7c0e28b7a653d593d3750fa3f2fe25b
EGREX <kirilmikov591@gmail.com>
2021-06-11 02:04:19 +0500

    update main menu and ingame menu

--- Game/Assets/Scripts/Panel.cs
+++ Game/Assets/Scripts/Panel.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Panel : MonoBehaviour
 {
+    public GameObject Buttons;
+    public GameObject Levels;
     public void PlayPressed()
     {
         SceneManager.LoadScene("Level1");
@@ -14,9 +16,31 @@ public class Panel : MonoBehaviour
         Application.Quit();
         Debug.Log("Exit pressed!");
     }
-
-    public void AuthorsPressed()
+    public void FirstLevel()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Level1");
+    }
+    public void SecondLevel()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void ThirdLevel()
+    {
+        SceneManager.LoadScene("Level3");
+    }    
+    public void FourthLevel()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+    public void LevelPressed()
+    {
+        Buttons.SetActive(false);
+        Levels.SetActive(true);
+    }
+
+    public void BackPressed()
+    {
+        Buttons.SetActive(true);
+        Levels.SetActive(false);
     }
 }
